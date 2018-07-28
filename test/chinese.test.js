f273de748d0fd2b17b24af860dae33cda1d15141
siwi chao <siwilizhao@gmail.com>
2018-07-28 10:29:01 +0800

    fix

--- test/chinese.test.js
+++ test/chinese.test.js
@@ -1,20 +1,39 @@
 const expect = require('chai').expect
 const chinese = require('../lib/models/chinese')
 describe('lib/models/chinese.js', () => {
-    // it('province', async() => {
-    //     const r = await chinese.province()    
-    //     console.log(r)
-    // });
-    // it('city', async () => {
-    //     const r = await chinese.city('山西')
-    //     console.log(r)
-    // });
+    it('province', async() => {
+        const r = await chinese.province()    
+        console.log(r)
+    });
+    it('city', async () => {
+        const r = await chinese.city()
+        console.log(r)
+    });
     // it('getProvinceByCity', async () => {
     //     const r = await chinese.getProvinceByCity('长安市')
     //     console.log(r)
     // });
-    it('code', async() => {
-        const r = await chinese.code()
-        console.log(r)
-    });
+    // it('code', async() => {
+    //     const r = await chinese.code()
+    //     console.log(r)
+    // });
+    // it('username', async() => {
+    //     for (let i = 0; i < 10000; i++) {
+    //         const r = await chinese.username()
+    //         console.log(r)
+    //     }
+
+    // });
+    // it('firstname', async() => {
+    //     const r = await chinese.firstName()
+    //     console.log(r)
+    // });
+    // it('lastname', async() => {
+    //     const r = await chinese.lastName()
+    //     console.log(r)
+    // });
+    // it('password', async() => {
+    //     const r = await chinese.password()
+    //     console.log(r)
+    // });
 });
