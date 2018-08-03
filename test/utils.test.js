9993725252be3d12b1a57721185f94ca783c6054
zhao wen qiang <13143410521@163.com>
2018-08-03 20:28:46 +0800

    fix

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,10 +3,10 @@ describe('utils.js', () => {
     const utils = require('../lib/utils/')
     it('random',async () => {
         const length = 34
-        const r = await utils.random(34)
+        const r = await utils.randomIndex(34)
         console.log(r)
         expect(r).to.be.a('number')
-        const f = await utils.random()
+        const f = await utils.randomIndex()
         console.log(f)
         expect(f).to.equal(false)
     });
